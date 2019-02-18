b485100e0d7a1f56e053494d13221f3e9bad445b
Putra <com.owl.minerva@gmail.com>
2019-02-18 14:33:58 +0700

    new update

--- screens/AddClientScreen.js
+++ screens/AddClientScreen.js
@@ -1,39 +1,7 @@
 import React from 'react';
-import {WebView, View, ScrollView} from 'react-native';
-import {Spinner} from 'native-base';
-import Header from '../navigation/HeaderNavigationBar';
-import Styles from '../constants/Styles';
 
-export default class HomeScreen extends React.Component {
-  constructor(props) {
+export default class AddClientScreen extends React.Component {
+  constructor(props){
     super(props);
-    this.state = {
-      isLoading: true
-    }
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 1000);
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return (<View style={[
-          Styles.container, {
-            backgroundColor: '#69594d'
-          }
-        ]}>
-        <Header {...this.props} iconNameIos='ios-home' iconNameAndroid='md-home'/>
-        <View style={[Styles.container, Styles.justifyContentCenter]}>
-          <Spinner color='#ffa81d'/>
-        </View>
-      </View>);
-    }
-    return (<View style={Styles.container}>
-      <Header {...this.props} iconNameIos='ios-home' iconNameAndroid='md-home'/>
-      <View style={[Styles.container, Styles.justifyContentCenter]}></View>
-    </View>);
   }
 }
